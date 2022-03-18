0a5227755a3c47fe1b6358b471935c1eb0096c59
Jonathan2307 <gratigny.j@me.com>
2022-03-18 13:14:41 +0100

    systeme de navigation OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,31 @@
-import React, { FunctionComponent, useState, useEffect } from "react";
-import Pokemon from "./models/pokemon";
-import POKEMONS from "./models/mock-pokemon";
+import React, { FunctionComponent } from "react";
+import PokemonList from "./pages/pokemon-list";
+import PokemonDetail from "./pages/pokemon-detail";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+
 
 
 const App: FunctionComponent = () => {
-    const [pokemons, setPokemons] = useState<Pokemon[]>([])
-
-    useEffect(() => {
-        setPokemons(POKEMONS);
-    }, []); //le tableau vide évite de declencher le hook d'effet a chaque modification du composant
-
+  
     return (
-        <div>
-            <h1 className="center">Pokedex</h1>
-            <div className="container">
-                <div className="row">
-                    {pokemons.map(({ id, name, picture, created }) => (
-                        <div className="col s6 m4" key={id}>
-                            <div className="card horizontal">
-                                <div className="card-image">
-                                    <img src={picture} alt={name} />
-                                </div>
-                                <div className="card-stacked">
-                                    <div className="card-content">
-                                        <p>{name}</p>
-                                        <p><small>{created.toString()}</small></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+<Router>
+    <div>
+        {/* La barre de navigation commune a toutes les pages */}
+        <nav>
+            <div className="nav-wrapper teal">
+                <Link to="/" className="brand-logo center">Pokedex</Link>
+
             </div>
-        </div>
+        </nav>
+        {/* { la gestion des routes} */}
+        <Switch>
+            <Route exact path="/" component={PokemonList} />
+            <Route exact path="/pokemons" component={PokemonList} />
+            <Route path="/pokemons/:id" component={PokemonDetail} />
+        </Switch>
+    </div>
+</Router>
     )
 }
 
