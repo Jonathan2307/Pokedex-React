6be58cdf39283f12a27f794ae4219215d5475c82
Jonathan2307 <gratigny.j@me.com>
2022-03-24 15:16:34 +0100

    traitement du formulaire et validation des donnees

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,31 @@ import PokemonList from "./pages/pokemon-list";
 import PokemonDetail from "./pages/pokemon-detail";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import PageNotFound from "./components/page-not-found";
-
-
-
+import PokemonEdit from "./pages/pokemon-edit";
 
 const App: FunctionComponent = () => {
-  
-    return (
-<Router>
-    <div>
+  return (
+    <Router>
+      <div>
         {/* La barre de navigation commune a toutes les pages */}
         <nav>
-            <div className="nav-wrapper teal">
-                <Link to="/" className="brand-logo center">Pokedex</Link>
-
-            </div>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo center">
+              Pokedex
+            </Link>
+          </div>
         </nav>
         {/* { la gestion des routes} */}
         <Switch>
-            <Route exact path="/" component={PokemonList} />
-            <Route exact path="/pokemons" component={PokemonList} />
-            <Route path="/pokemons/:id" component={PokemonDetail} />
-            <Route component={ PageNotFound } />
+          <Route exact path="/" component={PokemonList} />
+          <Route exact path="/pokemons" component={PokemonList} />
+          <Route exact path="/pokemons/edit/:id" component={PokemonEdit} />
+          <Route path="/pokemons/:id" component={PokemonDetail} />
+          <Route component={PageNotFound} />
         </Switch>
-    </div>
-</Router>
-    )
-}
+      </div>
+    </Router>
+  );
+};
 
-
-
-export default App
+export default App;
